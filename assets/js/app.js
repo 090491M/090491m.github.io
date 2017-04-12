0f7940e63f292d1a309687039f99164db689b213
Asep Irman <achmad.mercu@gmail.com>
2017-04-12 12:53:55 +0700

    Fix Responsive & Button to top

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,6 +31,28 @@
 
 			self.dashSmooth();
 			self.nav();
+			self.scrollTrigger();
+		},
+
+
+		scrollTrigger: function() {
+			$(window).on('scroll', function() {
+				var offset = 0; //Titik awal Scroll
+				var tinggi_layar = $(window).height();	//Menghitung Tinggi Layar
+				var tinggi_maks_scroll = $('body').height() + tinggi_layar; //Menghitung Tinggi Scroll
+				var posisi_scroll_y = window.pageYOffset; //Menghitung Posisi Scroll Y
+
+				var posisi_scroll = $('#materi').offset().top; //Posisi Trigger
+				var titik_batas_scroll = posisi_scroll - (tinggi_layar * offset);
+				var titik_batas = posisi_scroll_y > titik_batas_scroll; 
+				var sisa_titik_batas = tinggi_maks_scroll <= posisi_scroll_y && !titik_batas;
+
+			    if (titik_batas || sisa_titik_batas) {
+			        $('.button--to-top').addClass("button--to-top--show");
+			    }else{
+			        $('.button--to-top').removeClass("button--to-top--show");
+			    }
+			});
 		},
 
 		dashSmooth: function() {
